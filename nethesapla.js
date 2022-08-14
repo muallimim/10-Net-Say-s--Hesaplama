5466aca3e280a4df2d9fb7b5c1d95d6eedd6d87d
muallimim <muallimim@yahoo.com>
2022-08-14 20:48:50 +0300

    proje tamamlandı.

---
+++ nethesapla.js
@@ -0,0 +1,16 @@
+document.getElementById("btnNetHesapla").addEventListener("click", netHesapla);
+
+function netHesapla(){
+    event.preventDefault();
+    
+    var true_count;
+    var false_count;
+    var net_count;
+
+    true_count=document.getElementById("true").value;
+    false_count=document.getElementById("false").value;
+
+    net_count = true_count - (false_count/4);
+
+    document.getElementById("net").value = net_count;
+}
